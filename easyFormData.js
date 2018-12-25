a27fb3e43720fbe474bbbce00a56c25305d80ae0
hont127 <hont127@163.com>
2018-12-25 09:36:52 +0800

    Signed-off-by: hont127 <hont127@163.com>

---
+++ easyFormData.js
@@ -0,0 +1,28 @@
+function EasyFormData() {
+}
+
+EasyFormData.prototype.kv_pair = [];
+
+EasyFormData.prototype.append = function(key, value) {
+  this.kv_pair.push([key, value]);
+};
+
+EasyFormData.prototype.toString = function() {
+	var sb = [];
+	
+	for(var i = 1; i < this.kv_pair.length; i++) {
+		var key = this.kv_pair[i][0];
+		var value = this.kv_pair[i][1];
+		
+		key = escape(key);
+		value = escape(value);
+		
+		sb.push(key+"="+value);
+		
+		if(i+1 < this.kv_pair.length) {
+			sb.push("&");
+		}
+	}
+	
+	return sb.join("");
+}
